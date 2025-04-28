f3f46e09f435483e6915571bd5682f6ab3bf1188
Kelly Speelman <kellydejonge@hotmail.nl>
2025-04-28 22:48:27 +0200

    test array and list

--- TestPython.py
+++ TestPython.py
@@ -1,6 +1,19 @@
-num1 = float(input("typ een nummer: "))
-num2 = int(input("typ een nummer: "))
+# num1 = float(input("typ een nummer: "))
+# num2 = int(input("typ een nummer: "))
+#
+# sum = num1 + num2
+#
+# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
-sum = num1 + num2
+input_int_array = [int(x) for x in input("Enter the integer elements of list(Space-Separated): ").split()]
 
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print("array:", input_int_array)
+
+
+# input size of the list
+n = int(input("Enter the size of list : "))
+# store integers in a list using map, split and strip functions
+lst = list(map(int, input(
+    "Enter the integer elements of list(Space-Separated): ").strip().split()))[:n]
+print('The list is:', lst)   # printing the list
+
