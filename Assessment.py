b63081f4c1461c1f81a0f162013b2b969cd4d948
Kelly Speelman <kellydejonge@hotmail.nl>
2025-04-29 11:49:34 +0200

    Error handeling begin

--- Assessment.py
+++ Assessment.py
@@ -1,16 +1,33 @@
-input_rows = int(input("Enter the number of rows : "))
+def get_number():
+    while True:
+        try:
+            return int(input("Enter the number of rows : "))
+        except ValueError:
+            print("That was not a number")
+
+
+input_rows = get_number()
 list_of_lists = []
 diagonals1 = 0
 diagonals2 = 0
 
-def Create_List_of_Lists():
+def create_List_of_Lists():
     global list_of_lists
     for new_row in range(input_rows):
-        input_int_list = list(map(int, input(
-            "Enter the integer elements of list(Space-Separated): ").strip().split()))[:input_rows]
+        input_int_list = create_List()
         list_of_lists.append(input_int_list)
 
-def Calculate_Diagonals():
+
+def create_List():
+    while True:
+        try:
+            return list(map(int, input(
+                "Enter the integer elements of list(Space-Separated): ").strip().split()))[:input_rows]
+        except ValueError:
+            print("That was not a number")
+
+
+def calculate_Diagonals():
     global input_rows
     global diagonals1
     global diagonals2
@@ -20,13 +37,14 @@ def Calculate_Diagonals():
         diagonals2 += int(row[input_rows-row_count-1])
         row_count += 1
 
-def Calculate_diffrens():
+
+def calculate_diffrens():
     global diagonals1
     global diagonals2
     return diagonals1 - diagonals2
 
-Create_List_of_Lists()
-Calculate_Diagonals()
+create_List_of_Lists()
+calculate_Diagonals()
 print('The sum diagonal1 = ', diagonals1)
 print('The sum diagonal2 = ', diagonals2)
-print('differens: ', Calculate_diffrens())
+print('differens: ', calculate_diffrens())
