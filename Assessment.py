600c5479537f67fb94fbe0118594dc345820c254
Kelly Speelman <kellydejonge@hotmail.nl>
2025-04-29 11:32:25 +0200

    maak de lijsten aan in de lijst

--- Assessment.py
+++ Assessment.py
@@ -1,17 +1,14 @@
 input_rows = int(input("Enter the number of rows : "))
-
-input_int_list1 = list(map(int, input(
-    "Enter the integer elements of list(Space-Separated): ").strip().split()))[:input_rows]
-print("array:", input_int_list1)
-input_int_list2 = list(map(int, input(
-    "Enter the integer elements of list(Space-Separated): ").strip().split()))[:input_rows]
-print("array:", input_int_list2)
-input_int_list3 = list(map(int, input(
-    "Enter the integer elements of list(Space-Separated): ").strip().split()))[:input_rows]
-print("list:", input_int_list3)
-list_of_lists = [input_int_list1, input_int_list2, input_int_list3]
+list_of_lists = []
 diagonals1 = 0
 diagonals2 = 0
+
+def Create_List_of_Lists():
+    global list_of_lists
+    for new_row in range(input_rows):
+        input_int_list = list(map(int, input(
+            "Enter the integer elements of list(Space-Separated): ").strip().split()))[:input_rows]
+        list_of_lists.append(input_int_list)
 
 def Calculate_Diagonals():
     global input_rows
@@ -22,14 +19,13 @@ def Calculate_Diagonals():
         diagonals1 += int(row[row_count])
         diagonals2 += int(row[input_rows-row_count-1])
         row_count += 1
-        print('1: ', diagonals1)
-        print('2: ', diagonals2)
 
 def Calculate_diffrens():
     global diagonals1
     global diagonals2
     return diagonals1 - diagonals2
 
+Create_List_of_Lists()
 Calculate_Diagonals()
 print('The sum diagonal1 = ', diagonals1)
 print('The sum diagonal2 = ', diagonals2)
